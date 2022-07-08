e96ac7f74d0446c85610650ab7ea837ade8183eb
tarunfy <tarunsharma8920@gmail.com>
2022-07-09 00:37:17 +0530

    created booking page ui

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import Calendar from "react-calendar";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import "react-calendar/dist/Calendar.css";
 
 const Booking = () => {
@@ -19,12 +20,24 @@ const Booking = () => {
           alt="avatar"
           className="h-14 w-14 rounded-full mb-3"
         />
-        <h1 className="text-xl font-medium text-white/90">
+        <h1 className="text-2xl font-medium text-white/90">
           {currentUser.user_metadata.name}
         </h1>
         <p className="text-lg text-white/80">
           {currentUser.user_metadata.email}
         </p>
+        <div className="mt-10 space-y-2">
+          <h2 className="text-white text-xl">Event 1</h2>
+          <p className="text-white/90 font-light">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem
+            magnam maxime. Corporis enim delectus ullam consequatur quam
+            pariatur necessitatibus?
+          </p>
+          <div className="flex w-full items-center text-secondary-300">
+            <AccessTimeIcon className="mr-2 t" />
+            <p className="font-normal text-xl">30 mins</p>
+          </div>
+        </div>
       </div>
       <div className="w-[60%] p-10 h-full rounded-tr-xl rounded-br-xl space-y-6">
         <h1 className="text-2xl font-bold ">Select a Date</h1>
