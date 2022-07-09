b817a5a6c45dfb885c83d2133155db765d1ebd01
tarunfy <tarunsharma8920@gmail.com>
2022-07-09 13:41:22 +0530

    fixed a bug on booking page

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -16,15 +16,15 @@ const Booking = () => {
     <div className="max-w-[800px] font-Outfit my-28 shadow-custom rounded-xl h-[500px] flex items-center justify-between max-h-screen mx-auto">
       <div className="w-2/5 p-10 bg-secondary-500 h-full rounded-tl-xl rounded-bl-xl">
         <img
-          src={currentUser.user_metadata?.picture}
+          src={currentUser?.user_metadata?.picture}
           alt="avatar"
           className="h-14 w-14 rounded-full mb-3"
         />
         <h1 className="text-2xl font-medium text-white/90">
-          {currentUser.user_metadata.name}
+          {currentUser?.user_metadata.name}
         </h1>
         <p className="text-lg text-white/80">
-          {currentUser.user_metadata.email}
+          {currentUser?.user_metadata.email}
         </p>
         <div className="mt-10 space-y-2">
           <h2 className="text-white text-xl">Event 1</h2>
